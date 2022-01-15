554bd3ea77980b9b4f94d9f8c87b71bc7fa0c1c0
lpercivalDEV <lpercival.dev@gmail.com>
2022-01-14 20:37:50 -0500

    Add blurb

--- update_21/illaisland/src/About.js
+++ update_21/illaisland/src/About.js
@@ -2,7 +2,9 @@ function About() {
   return (
     <div>
       <a href="mailto:lpercival.dev@gmail.com" class="center">
-        ABOUT ME - IM AWESOME PAY ME MONIES SO I CAN DO MORE AWESOME STUFF
+        illa island is an umbrella under which all my creative efforts exist. whether it's music, writings, events, or coding - all of them are part of illa island. no one is just one thing and I'm no exception - I code, I make art, and music, and create experiences for people to enjoy and make people think. things that represent my whole self and my culture.
+
+        bienvenidos a illa island. go get lost.
       </a>
     </div>
   );
